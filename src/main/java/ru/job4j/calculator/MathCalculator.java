4e615be562ced76654c8088a91213d9b26f4d5e7
Daniyar <aldanov2002@bk.ru>
2023-04-26 18:34:47 +0600

    Модульные тесты. [#159435]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+public class MathCalculator {
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double addAndDivide(double first, double second) {
+        return add(first, second)
+                + divide(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + addAndDivide(30, 5));
+    }
+}
