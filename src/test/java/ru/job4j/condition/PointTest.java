c9c896178f720cd9f636ec929da32b5c61f5c374
Daniyar <aldanov2002@bk.ru>
2023-04-27 17:29:19 +0600

    Тесты для задачи "идеальный вес" [#159458]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -13,29 +13,39 @@ public class PointTest {
         int y2 = 0;
         double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
-
-        expected = 2.8284271247461903;
-        x1 = 0;
-        y1 = 2;
-        x2 = 2;
-        y2 = 0;
-        out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-
-        expected = 0;
-        x1 = 2;
-        y1 = 0;
-        x2 = 2;
-        y2 = 0;
-        out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-
-        expected = 2.23606797749979;
-        x1 = 1;
-        y1 = 3;
-        x2 = 2;
-        y2 = 5;
-        out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
     }
+
+        @Test
+        public void when02to20then2() {
+            double expected = 2.8284271247461903;
+            int x1 = 0;
+            int y1 = 2;
+            int x2 = 2;
+            int y2 = 0;
+            double out = Point.distance(x1, y1, x2, y2);
+            Assert.assertEquals(expected, out, 0.01);
+        }
+
+        @Test
+        public void when20to20then2() {
+            double expected = 0;
+            int x1 = 2;
+            int y1 = 0;
+            int x2 = 2;
+            int y2 = 0;
+            double out = Point.distance(x1, y1, x2, y2);
+            Assert.assertEquals(expected, out, 0.01);
+        }
+
+        @Test
+        public void when13to25then2() {
+            double expected = 2.23606797749979;
+            int x1 = 1;
+            int y1 = 3;
+            int x2 = 2;
+            int y2 = 5;
+            double out = Point.distance(x1, y1, x2, y2);
+            Assert.assertEquals(expected, out, 0.01);
+        }
+
 }
