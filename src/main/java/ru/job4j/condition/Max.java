b7b35a8d15998d06e8e9904ab5caf2839da91a8b
Daniyar <aldanov2002@bk.ru>
2023-05-01 12:01:41 +0600

    Тернарное сравнение [#189 #413324]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,12 +2,23 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+        return left > right ? left : right;
+    }
+
+    public static int min(int left, int right) {
+        return left < right ? left : right;
+    }
+
+    public static int isEqual(int left, int right) {
+        return left == right ? left : right;
     }
 
     public static void main(String[] args)  {
         int result = Max.max(5, 2);
         System.out.println(result);
+        int result1 = Max.min(5, 2);
+        System.out.println(result1);
+        int result2 = Max.isEqual(5, 2);
+        System.out.println(result2);
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -12,4 +12,21 @@ public class MaxTest {
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
+    @Test
+    public void whenMin1To2Then1() {
+        int left = 1;
+        int right = 2;
+        int result = Max.min(left, right);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenIsEqual1To2Then2() {
+        int left = 1;
+        int right = 2;
+        int result = Max.isEqual(left, right);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
 }
