704ae9c6d19e40432112488914002ef99558731d
Daniyar <aldanov2002@bk.ru>
2023-05-04 14:44:23 +0600

    Заполнение массива. [#226300 #414054]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,30 +3,27 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
 
-        String[] names = new String[100500];
+        String[] names = new String[4];
         names[0] = "Daniyar Aldanov";
         names[1] = "Petr Arsentev";
         names[2] = "Егор Бугаенко";
         names[3] = "Олег Чергасов";
-        names[4] = "Ivan Petrenko";
 
         String dani = names[0];
         String petr = names[1];
         String egor = names[2];
         String oleg = names[3];
-        String ivan = names[4];
+
         System.out.println(names[0]
                 + " \n" + names[1]
                 + " \n" + names[2]
                 + " \n" + names[3]
-                + " \n" + names[4]
                 + " \n");
 
         System.out.println(dani
                 + " \n" + petr
                 + " \n" + egor
-                + " \n" + oleg
-                + " \n" + ivan);
+                + " \n" + oleg);
     }
 
 }
