22a91da295ca6e245d56dd43ed7ee63f1f543ce9
Daniyar <aldanov2002@bk.ru>
2023-05-07 18:14:56 +0600

    Выигрышные комбинации в сокобан [#53859]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,8 +3,8 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        for (int j = 0; j < board[row].length; j++) {
-            if (board[row][j] != ('X')) {
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != ('X')) {
                 result = false;
                 break;
             }
@@ -29,5 +29,16 @@ public class MatrixCheck {
             rsl[index] = board[index][index];
         }
         return rsl;
+    }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (monoHorizontal(board, i) || monoVertical(board, i)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 }
