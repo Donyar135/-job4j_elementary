32de815bc88ef1eb1275b787d67cc66f58091b17
Daniyar <aldanov2002@bk.ru>
2023-06-23 13:08:36 +0600

    2.0. Рефакторинг - Расстояние между точками. [#122587 # [#122587]]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,44 +7,36 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
         @Test
         public void when02to20then2dot83() {
             double expected = 2.83;
-            int x1 = 0;
-            int y1 = 2;
-            int x2 = 2;
-            int y2 = 0;
-            double out = Point.distance(x1, y1, x2, y2);
+            Point a = new Point(0, 2);
+            Point b = new Point(2, 0);
+            double out = a.distance(b);
             Assert.assertEquals(expected, out, 0.01);
         }
 
         @Test
         public void when20to20then0() {
             double expected = 0;
-            int x1 = 2;
-            int y1 = 0;
-            int x2 = 2;
-            int y2 = 0;
-            double out = Point.distance(x1, y1, x2, y2);
+            Point a = new Point(2, 0);
+            Point b = new Point(2, 0);
+            double out = a.distance(b);
             Assert.assertEquals(expected, out, 0.01);
         }
 
         @Test
         public void when13to25then2dot24() {
             double expected = 2.24;
-            int x1 = 1;
-            int y1 = 3;
-            int x2 = 2;
-            int y2 = 5;
-            double out = Point.distance(x1, y1, x2, y2);
+            Point a = new Point(1, 3);
+            Point b = new Point(2, 5);
+            double out = a.distance(b);
             Assert.assertEquals(expected, out, 0.01);
         }
 
